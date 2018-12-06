ede68d0173cb29455d8e67f7cc718802aa264dcc
Sandip Vaghasiya <skv4733@gmail.com>
2018-12-06 17:32:34 -0600

    Add files via upload

--- WeatherEntry.java
+++ WeatherEntry.java
@@ -1,1 +1,26 @@
+package com.javasans.weather;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class WeatherEntry
+	{
+		//Fields to be used from input files
+		Date when;
+		float pressure;
+		float humidity;
+		
+		 public String toString() 
+			 {
+				 DateFormat format=new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+				 
+				return ("Data for: "+format.format(when)
+				
+						+" Barometric Pressure: "
+						+Float.toString(pressure)
+						+" Humidity: "+Float.toString(humidity));
+				 
+				 
+			 }
+	}
